035062eb60f0a750e431e707b3b7e083ab2b4d99
Lee-jeong-wook <126264218+Lee-jeong-wook@users.noreply.github.com>
2023-12-02 23:08:16 +0900

    first commit

---
+++ src/components/Some.tsx
@@ -0,0 +1,11 @@
+export const Some = () => {
+    const calculating = <T,>(param: T): T => {
+        return param;
+    };
+
+    return (
+        <>
+            {/* 여기에 JSX 코드를 추가하거나 필요에 따라 다른 요소로 변경하세요. */}
+        </>
+    );
+};
